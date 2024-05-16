390b70014d6d87b4d02b2cc89476a30fc2eb42c5
Faruk Turgut <farukturgutiu@gmail.com>
2024-05-17 01:24:50 +0300

    Initial Commit

---
+++ Moviedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moviedex
+//
+//  Created by Faruk Turgut on 17.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
