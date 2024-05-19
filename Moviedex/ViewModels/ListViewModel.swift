2950332524fa0fbb9086df03462078c73025a30b
Faruk Turgut <farukturgutiu@gmail.com>
2024-05-19 22:54:18 +0300

    Separate view model logic

--- Moviedex/ViewModels/ListViewModel.swift
+++ Moviedex/ViewModels/ListViewModel.swift
@@ -23,41 +23,17 @@ class ListViewModel {
         let group = DispatchGroup()
         
         group.enter()
-        fetchVerticalListData {
+        verticalListViewModel.fetch {
             group.leave()
         }
         
         group.enter()
-        fetchHorizontalListData {
+        horizontalListViewModel.fetch {
             group.leave()
         }
         
         group.notify(queue: .main) {
             print("both ended")
-        }
-    }
-    
-    private func fetchVerticalListData(completion: @escaping () -> Void) {
-        provider.search(by: "test", page: 1) { [weak self] result in
-            switch result {
-            case let .success(response):
-                self?.verticalListViewModel.update(contents: response.search)
-            case .failure(_):
-                break
-            }
-            completion()
-        }
-    }
-    
-    private func fetchHorizontalListData(completion: @escaping () -> Void) {
-        provider.search(by: "another", page: 1) { [weak self] result in
-            switch result {
-            case let .success(response):
-                self?.horizontalListViewModel.update(contents: response.search)
-            case .failure(_):
-                break
-            }
-            completion()
         }
     }
     
--- Moviedex/ViewModels/VerticalListViewModel.swift
+++ Moviedex/ViewModels/VerticalListViewModel.swift
@@ -9,15 +9,30 @@ import Foundation
 
 class VerticalListViewModel {
     
+    private let provider: Provider
+    
     var onChanged: (([Content]) -> Void)?
     
     var contents: [Content]
     
-    init(contents: [Content] = []) {
+    init(contents: [Content] = [], provider: Provider = APIProvider()) {
+        self.provider = provider
         self.contents = contents
     }
     
-    func update(contents: [Content]) {
+    func fetch(completion: @escaping () -> Void) {
+        provider.search(by: "test", page: 1) { [weak self] result in
+            switch result {
+            case let .success(response):
+                self?.update(contents: response.search)
+            case .failure(_):
+                break
+            }
+            completion()
+        }
+    }
+    
+    private func update(contents: [Content]) {
         self.contents = contents
         onChanged?(self.contents)
     }
